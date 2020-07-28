a7173e7f424f6c81dcaad09306acb7fe2f291991
张铭 <mingzhang@chutianyun.gov.cn>
2020-07-28 14:02:11 +0800

    BeanConverter

--- src/main/java/com/zhangm/easyutil/BeanConverter.java
+++ src/main/java/com/zhangm/easyutil/BeanConverter.java
@@ -4,18 +4,18 @@ import lombok.extern.log4j.Log4j2;
 
 import java.lang.reflect.Field;
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * @Author zhangming
  * @Date 2020/5/29 15:38
  */
-@Log4j2
-public class BeanConverter {
+public interface BeanConverter {
 
-    public static <T> T convert(Map<String, ?> map, Class<T> tClass) {
+    static <T> T convert(Map<String, ?> map, Class<T> tClass) {
         try {
-            T t = tClass.newInstance();
+            T t = tClass.getDeclaredConstructor().newInstance();
             Field[] fields = tClass.getDeclaredFields();
             for (Field field : fields) {
                 field.setAccessible(true);
@@ -28,8 +28,40 @@ public class BeanConverter {
             }
             return t;
         } catch (Exception ex) {
-            log.error(ex.getMessage());
+            System.out.println(Strings.f("error: {}", ex.getMessage()));
             throw new RuntimeException(ex.getMessage());
         }
+    }
+
+    /**
+     * 将父类的属性赋值给子类
+     * @param father
+     * @param fatherClazz
+     * @param son
+     * @param sonClazz
+     * @param <T1>
+     * @param <T2>
+     */
+    static <T1, T2 extends T1> void transfer(T1 father, Class<T1> fatherClazz, T2 son, Class<? extends T2> sonClazz) {
+        try {
+            Field[] fatherFields = fatherClazz.getDeclaredFields();
+            Map<String, Field> sonFieldMap = new HashMap<>();
+            Field[] sonFields = sonClazz.getDeclaredFields();
+            for (Field field : sonFields) {
+                sonFieldMap.put(field.getName(), field);
+            }
+            for (Field field : fatherFields) {
+                field.setAccessible(true);
+                Object value = field.get(father);
+                Field sonField = sonFieldMap.get(field.getName());
+                if (sonField != null) {
+                    sonField.setAccessible(true);
+                    sonField.set(son, value);
+                }
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+
     }
 }
