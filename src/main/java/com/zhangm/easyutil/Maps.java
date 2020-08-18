1a776d717b036b2e8cd52a444ca6834f59e7f2f0
ming <1570977353@qq.com>
2020-08-18 11:43:28 +0800

    add Maps api

--- src/main/java/com/zhangm/easyutil/Maps.java
+++ src/main/java/com/zhangm/easyutil/Maps.java
@@ -1,10 +1,13 @@
 package com.zhangm.easyutil;
 
+import com.google.common.collect.Lists;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -21,18 +24,20 @@ public class Maps {
         private V value;
     }
 
-    public static <K, V> Entry newEntry(K key, V value) {
-        return Entry.builder()
+    public static <K, V> Entry<K, V> newEntry(K key, V value) {
+        return Entry.<K, V>builder()
                 .key(key)
                 .value(value)
                 .build();
     }
 
-    public static <K, V> Map<K, V> newHashMap(Entry<K, V>... entries) {
-        Map<K, V> map = new HashMap<>();
+    @SafeVarargs
+    public static <K, V> HashMap<K, V> newHashMap(Entry<K, V>... entries) {
+        HashMap<K, V> map = new HashMap<>();
         for (Entry<K, V> entry : entries) {
             map.put(entry.getKey(), entry.getValue());
         }
         return map;
     }
+
 }
