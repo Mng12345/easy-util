a975eadf071ab9a3f9dc7cd455b7e161100db58c
ming <1570977353@qq.com>
2020-08-19 09:46:10 +0800

    add wrap exception api

--- src/main/java/com/zhangm/easyutil/Functions.java
+++ src/main/java/com/zhangm/easyutil/Functions.java
@@ -1,5 +1,8 @@
 package com.zhangm.easyutil;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+
 /**
  * @Author zhangming
  * @Date 2020/8/6 14:51
@@ -8,5 +11,89 @@ public class Functions {
 
     interface VoidFuncNoArg {
         void apply();
+    }
+
+    static <T> Consumer<T> wrapConsumer(Consumer<T> consumer) {
+        return t -> {
+            try {
+                consumer.accept(t);
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        };
+    }
+
+    static <T1, T2> Streams.Consumer<T1, T2> wrapConsumer(Streams.Consumer<T1, T2> consumer) {
+        return (t1, t2) -> {
+            try {
+                consumer.accept(t1, t2);
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        };
+    }
+
+    static <T1, T2, T3> Streams.Consumer2<T1, T2, T3> wrapConsumer(Streams.Consumer2<T1, T2, T3> consumer) {
+        return (t1, t2, t3) -> {
+            try {
+                consumer.accept(t1, t2, t3);
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        };
+    }
+
+    static <T1, T2, T3, T4> Streams.Consumer3<T1, T2, T3, T4> wrapConsumer(Streams.Consumer3<T1, T2, T3, T4> consumer) {
+        return (t1, t2, t3, t4) -> {
+            try {
+                consumer.accept(t1, t2, t3, t4);
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        };
+    }
+
+    static <T, R> Function<T, R> wrapFunction(Function<T, R> function) {
+        return t -> {
+            try {
+                return function.apply(t);
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        };
+    }
+
+    static <T1, T2, R> Streams.Function<T1, T2, R> wrapFunction(Streams.Function<T1, T2, R> function) {
+        return (t1, t2) -> {
+            try {
+                return function.apply(t1, t2);
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        };
+    }
+
+    static <T1, T2, T3, R> Streams.Function2<T1, T2, T3, R> wrapFunction(Streams.Function2<T1, T2, T3, R> function) {
+        return (t1, t2, t3) -> {
+            try {
+                return function.apply(t1, t2, t3);
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        };
+    }
+
+    static <T1, T2, T3, T4, R> Streams.Function3<T1, T2, T3, T4, R> wrapFunction(Streams.Function3<T1, T2, T3, T4, R> function) {
+        return (t1, t2, t3, t4) -> {
+            try {
+                return function.apply(t1, t2, t3, t4);
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+
     }
 }
--- src/main/java/com/zhangm/easyutil/Streams.java
+++ src/main/java/com/zhangm/easyutil/Streams.java
@@ -3,6 +3,7 @@ package com.zhangm.easyutil;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
