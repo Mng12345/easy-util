30b3cb2fe586f2319d7895142efde657a4b76ee4
张铭 <1570977353@qq.com>
2020-08-29 18:04:32 +0800

    add FileUtil

---
+++ src/main/java/com/zhangm/easyutil/FileUtil.java
@@ -0,0 +1,28 @@
+package com.zhangm.easyutil;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+/**
+ * @Author zhangming
+ * @Date 2020/8/29 17:58
+ */
+public interface FileUtil {
+
+    static String read(File file) {
+        if (!file.isFile()) {
+            throw new RuntimeException(Strings.f("{} must be file!", file.getAbsolutePath()));
+        }
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+            return stringBuilder.toString();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
--- src/main/java/com/zhangm/easyutil/Streams.java
+++ src/main/java/com/zhangm/easyutil/Streams.java
@@ -4,6 +4,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -98,4 +99,5 @@ public interface Streams {
     static <T> Optional<T> find(Stream<T> stream, Predicate<T> function) {
         return stream.filter(function).findFirst();
     }
+
 }
